e3bffcdbd59175573c4b1ecee4dce717a1351f75
priyankajatale <110655362+priyankajatale@users.noreply.github.com>
2022-08-05 13:01:48 +0530

    commit first commit

---
+++ Swapping.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+main()
+{
+	int a,b,temp;
+	printf("Enter A & B\n");
+	scanf("%d%d",&a,&b);
+	printf("Before Swapping a=%d and b=%d",a,b);
+	temp=a;
+	a=b;
+	b=temp;
+	printf("\nAfter Swapping a=%d and b=%d",a,b);
+	
+	
+}
